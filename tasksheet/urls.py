901f4ac28ec5a48dc0bbaa9257bbb4b88f7b4eff
QUILLARD <313st@bk.ru>
2023-11-02 00:23:04 +0600

    update

--- tasksheet/urls.py
+++ tasksheet/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 app_name = 'tasksheet'
 router = DefaultRouter()
@@ -17,6 +17,7 @@ urlpatterns = [
     path('api/v1/', include(router.urls), name='api_users'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 # urlpatterns = [
